a2dfe46b996e53ec6398558f6c3614fea9d06b12
soleova <gala.sole@yahoo.com>
2016-12-27 17:12:59 -0800

    povecan broj loptica i jos sitnih izmena

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
-#define MAX_BALLS 7
+#define MAX_BALLS 14
 #define RAND_NUM() ((double)rand()/RAND_MAX)
 
 typedef struct BALL{
    float x, y, r;
-   int state; 
-   /* State je indikator da li loptica treba da se crta. */
+   int state; /* State je indikator da li loptica treba da se crta. */ 
+   char size; /* Large, medium ili small velicina. */
+   
 }Ball;
  
 const char title[] = "Rebbubled";  
@@ -37,7 +38,7 @@ static float timer_x = 0.98;
 
 /* Brzina strele i "tajmera". */
 static float arrow_speed = 0.05; 
-static float timer_speed = 0.0003;
+static float timer_speed = 0.0005;
 
 /* Brzine loptica. */
 float vx[MAX_BALLS], vy[MAX_BALLS];
@@ -55,7 +56,10 @@ Ball* balls[MAX_BALLS];
 
 /* Granice. */  
 float ball_x_max, ball_x_min, ball_y_max, ball_y_min;
-float clip_area_x_left, clip_area_x_right, clip_area_y_bottom, clip_area_y_top;
+float clip_area_x_left = -1;
+float clip_area_x_right = 1;
+float clip_area_y_bottom = -1;
+float clip_area_y_top = 1;
 
 int refresh = 30;
 int score_value;
@@ -76,7 +80,7 @@ static int hits(float *x, float *y, float *r);
 /* Funkcija koja proverava da li je strela pogodila loptu.*/
 static int success(float *xpos, float *ypos, float *r);
 /* Funkcija koja pravi novu loptu. */
-static Ball* new_ball(float *xbr, float *ybr, float rbr, int state);
+static Ball* new_ball(float *xbr, float *ybr, float rbr, int state, char size);
 /* Funkcija koja inicijalizuje pocetne koordinate loptica i njihove brzine. */
 static void init_values();
 /* Funkcija koja inicijalizuje loptice. */
@@ -98,6 +102,10 @@ static void draw_timer();
 static void print_text(char *text, float x, float y);
 /*Funkcija koja ispisuje trenutni rezultat na ekranu. */
 static void print_score_value(float x, float y);
+/** Postavljaju se parametri svetla.*/
+static void init_light();
+/* Funkcija koja vraca random broj u nekom intervalu. */
+static double randf(double min, double max);
 
 void display(void) {
 
@@ -107,19 +115,8 @@ void display(void) {
    glMatrixMode(GL_MODELVIEW);   
    glLoadIdentity();          
 
-   /* Parametri svetla */
-   GLfloat light_ambient[] = { 0, 0, 0, 1 };
-   GLfloat light_diffuse[] = { 1, 1, 0.9, 1 };
-   GLfloat light_specular[] = { 1, 1, 1, 1 };
-   GLfloat model_ambient[] = { 0.4, 0.4, 0.4, 1 };
-   
-   glEnable(GL_LIGHTING);
-   glEnable(GL_LIGHT0);
-   glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-   glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-   glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-   glLightModelfv(GL_LIGHT_MODEL_AMBIENT, model_ambient);
-
+   init_light();
+ 
    /* Pozicija svetla. */
    GLfloat light_position[] = { 1, 1, 1, 0 };
 
@@ -176,7 +173,7 @@ void display(void) {
          print_text("YOU WON!", -0.15, 0.0);
       }
    }
- 
+    
    glMaterialfv(GL_FRONT, GL_AMBIENT, no_material);
    glMaterialfv(GL_FRONT, GL_DIFFUSE, blue_material_diffuse);
    glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
@@ -221,12 +218,33 @@ void display(void) {
    draw_timer(); 
 
    /*Ako je pogodjen igrac, izlazimo iz programa. */
-   if(hit_flag == 1){
+  /* if(hit_flag == 1){
       exit(1);   
-   }
+   }*/
    
    /* Postavlja se nova slika u prozor. */
    glutSwapBuffers(); 
+}
+
+static double randf(double min, double max){
+    return ((double)rand()/(RAND_MAX)) * abs(min - max) + min;
+}
+
+static void init_light(){
+ 
+   /* Parametri svetla */
+   GLfloat light_ambient[] = { 0, 0, 0, 1 };
+   GLfloat light_diffuse[] = { 1, 1, 0.9, 1 };
+   GLfloat light_specular[] = { 1, 1, 1, 1 };
+   GLfloat model_ambient[] = { 0.4, 0.4, 0.4, 1 };
+   
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+   glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+   glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+   glLightModelfv(GL_LIGHT_MODEL_AMBIENT, model_ambient);
+
 }
 
 static void init_values(){
@@ -235,32 +253,39 @@ static void init_values(){
    int i;
    for (i = 0; i < MAX_BALLS; i++) {
 
-     xpos[i] = RAND_NUM() - 0.5;
-     ypos[i] = RAND_NUM() - 0.5;
-     vx[i] = RAND_NUM() * 0.03 + 0.01;
-     vy[i] = RAND_NUM() * 0.06 + 0.05;
+     xpos[i] = randf(-0.8, 0.8);
+     ypos[i] = randf(-0.4, 0.7);
+     vx[i] = randf(0, 1) * 0.03 + 0.01;
+     vy[i] = randf(0, 1) * 0.07 + 0.05;
      
    }
 }
 
 static void init_balls(){
   
-   /* Lopticama dodeljujemo pozicije i brzine. */
-   
+   /* Lopticama dodeljujemo pozicije i brzine.
+      Moglo je bolje da se odradi, ako stignem bice doradjeno.
+      S obzirom da moraju da se pojave dve manje loptice posle
+      one koja je pogodjena, ovako mi je nekako bilo najlakse 
+      da ih inicijalizujem i da kasnije mogu lakse da proveravam
+      pogotke. 
+      0 1 2 3 4 5 6 7 8 9 10 11 12 13
+      l m s s m s s l m s  s  m  s  s */
+
    int i, k;
    for(i = 0, k = 0; i < MAX_BALLS; i++, k++){
    
-   /* Prva lopta ima najveci radijus. */
-     if(i == 0){
-        balls[i] = new_ball(&xpos[k], &ypos[k], big_radius, 1);
+   /* Lopte sa velicinom large. */
+     if(i == 0 || i == 7){
+        balls[i] = new_ball(&xpos[k], &ypos[k], big_radius, 1, 'l');
      }
-     /* Sledece dve imaju manji. */
-     if(i == 1 || i == 2){
-        balls[i] = new_ball(&xpos[k], &ypos[k], medium_radius, 0);
+     /* Lopte sa velicinom medium */
+     if(i == 1 || i == 4 || i == 8 || i == 11){
+        balls[i] = new_ball(&xpos[k], &ypos[k], medium_radius, 0, 'm');
      }
-     /* Poslednje 4 imaju najmanji. */
-     if(i > 2){
-        balls[i] = new_ball(&xpos[k], &ypos[k], small_radius, 0);
+     /* Lopte sa velicinom small */
+     if(i == 2 || i == 3 || i == 5 || i == 6 || i == 9 || i == 10 || i == 12 || i == 13){
+        balls[i] = new_ball(&xpos[k], &ypos[k], small_radius, 0, 's');
      }
    }
 
@@ -338,6 +363,7 @@ static void draw_ball(){
         }  
    }
    glPopMatrix();
+
 }
 
 static void draw_background(){
@@ -346,11 +372,13 @@ static void draw_background(){
    /* TO DO: iskoristiti animation_parameter za neko fino menjanje boje. */
    glPushMatrix();
     glBegin(GL_QUADS);	
-     glColor3f(0.1, 0.4, 1.0);
-     glVertex2f(-1.0, -0.9);  
+     glColor3f(0.1, 0.4, 0.3);
+     glVertex2f(-1.0, -0.9);
+     glColor3f(1.0, 0.2, 0.5);  
      glVertex2f(1.0, -0.9);
-     glColor3f(1.0, 0.4, 0.2);
+     glColor3f(0.1, 0.7, 1.0);
      glVertex2f(1.0, 1.0);
+     glColor3f(0.4, 0.9, 0.7);
      glVertex2f(-1.0, 1.0);
     glEnd();
    glPopMatrix();
@@ -413,7 +441,7 @@ static void bounce(){
        ball_x_min = clip_area_x_left + balls[i]->r;
        ball_x_max = clip_area_x_right - balls[i]->r;
        ball_y_min = clip_area_y_bottom + 0.1 + balls[i]->r;
-       ball_y_max = clip_area_y_top/1.5 - balls[i]->r;
+       ball_y_max = clip_area_y_top/1.2 - balls[i]->r;
 
        /* Ponasanje lopti u zavisnosti od toga 
           da li su udarile u zid.*/
@@ -458,7 +486,7 @@ static void bounce(){
    glutPostRedisplay();
 
    animation_active = 1;
-}
+  }
 }
 
 static void shoot(void){
@@ -471,7 +499,7 @@ static void shoot(void){
    }
 }
 
-static Ball* new_ball(float *xbr, float *ybr, float rbr, int state){
+static Ball* new_ball(float *xbr, float *ybr, float rbr, int state, char size){
    
    /* Kreiramo novu loptu. */
    Ball* new_ball = (Ball*)malloc(sizeof(Ball));
@@ -483,6 +511,7 @@ static Ball* new_ball(float *xbr, float *ybr, float rbr, int state){
    new_ball->y = *ybr;
    new_ball->r = rbr;
    new_ball->state = state;
+   new_ball->size = size;
  
    return new_ball;
 }
@@ -516,37 +545,31 @@ static void timer(int value)
       loptice koje treba da se crtaju postavlja na 1. */
 
    int p;
-   for(p = 0; p<MAX_BALLS; p++){
+   for(p = 0; p < MAX_BALLS; p++){
       if(success(&balls[p]->x, &balls[p]->y, &balls[p]->r) && balls[p]->state == 1) {
-         if(p == 0){
-            balls[p]->state = 0;        // Ako je pogodjena prva loptica,
-            balls[p+1]->state = 1;      // treba da se pojave sledece dve manje.
-            balls[p+2]->state = 1; 
-	    score_value += 100;
-            num_balls_hit++;
-         }
-         if(p == 1){                 
-            balls[p]->state = 0;        // Ako je pogodjena prva manja loptica,
-            balls[p+2]->state = 1;      // treba da ostane druga manja loptica i da
-            balls[p+3]->state = 1;      // se pojave dve manje od prve. 
- 	    score_value += 150;
-            num_balls_hit++;
-         }
-         if(p == 2){                    
-            balls[p]->state = 0;        // Ako je pogodjena druga manja loptica.
-            balls[p+3]->state = 1;      // treba da ostane prva manja loptica i da
-            balls[p+4]->state = 1;      // se pojave dve manje od druge.  
-            score_value += 150;
-            num_balls_hit++;
-         }
-         if(p > 2){                      
-            balls[p]->state = 0;        // Ako su pogodjene najmanje loptice,
-            score_value += 200;          // ne crtamo nove loptice.
-            num_balls_hit++;
+                     
+            if(balls[p]->size == 'l'){    // Ako je pogodjena large lopta, onda se ona vise
+               balls[p]->state = 0;       // ne crta, a crtaju se dve medium loptice. 
+               balls[p+1]->state = 1;      
+               balls[p+4]->state = 1;
+	       score_value += 100;
+               num_balls_hit++;
+            }                 
+            if(balls[p]->size == 'm'){    // Ako je pogodjena medium lopta, onda se ona vise
+               balls[p]->state = 0;       // ne crta, a crtaju se dve medium loptice.
+               balls[p+1]->state = 1;
+               balls[p+2]->state = 1;  
+ 	       score_value += 150;
+               num_balls_hit++;
+            }
+            if(balls[p]->size == 's'){     //Ako je pogodjena small lopta, dalje ne crtamo loptice.
+               balls[p]->state = 0;        
+               score_value += 200;         
+               num_balls_hit++;
          }
                 
-         //printf("Pogodjena loptica broj %d!\n", p);
-          
+         /* Strela prestaje da se crta onda kada pogodi lopticu. */
+         arrow_animation = 0;
      }
   }
  
@@ -588,13 +611,13 @@ static void keyboard(unsigned char key, int x, int y){
    case 'd':
             /* Kretanje u desno sve dok ne dodjemo do granice. */
             if(animation_active && player_x <= clip_area_x_right - 0.05){
-            player_x += 0.02;
+            player_x += 0.03;
      	    } 
 	    break;
    case 'a':
             /* Kretanje u levo sve dok ne dodjemo do granice. */
 	    if(animation_active && player_x >= clip_area_x_left + 0.05){
-	    player_x -= 0.02;       
+	    player_x -= 0.03;       
 	    } 
 	    break;
    case 'f':
@@ -616,31 +639,14 @@ static void keyboard(unsigned char key, int x, int y){
 
 void reshape(int width, int height) {
 
-   if (height == 0) height = 1;    
-
-   GLfloat aspect = (float)width / (float)height;
- 
    glViewport(0, 0, width, height);
  
    glMatrixMode(GL_PROJECTION);  
    glLoadIdentity();           
 
-   if (width >= height) {
-       clip_area_x_left   = -1.0 * aspect;
-       clip_area_x_right  = 1.0 * aspect;
-       clip_area_y_bottom = -1.0;
-       clip_area_y_top    = 1.0;
-   } else {
-       clip_area_x_left   = -1.0;
-       clip_area_x_right  = 1.0;
-       clip_area_y_bottom = -1.0 / aspect;
-       clip_area_y_top    = 1.0 / aspect;
-   }
-   
    gluOrtho2D(clip_area_x_left, clip_area_x_right, clip_area_y_bottom, clip_area_y_top);
 
    /* Ne dozvoljavamo promene velicine prozora. */
-   /* Nisam sigurna koliko je ovo korektno, ali neka ostane ovako. */
    glutReshapeWindow(500, 500);
 
 }
@@ -665,7 +671,7 @@ int main(int argc, char** argv) {
    /* OpenGL inicijalizacija. */
    glClearColor (0.0, 0.0, 0.0, 0.0); 
    glEnable(GL_DEPTH_TEST);
- 
+  
    /* Registrovanje funkcija za obradu dogadjaja */
    glutIdleFunc(bounce);
    glutDisplayFunc(display);    
@@ -677,6 +683,9 @@ int main(int argc, char** argv) {
     animation_parameter = 0;
     animation_active = 0;
    
+   /* Postavlja se seed za generisanje random brojeva. */
+   srand(time(NULL));
+
    /* Inicijalizacija pocetnih koordinata i brzina loptica. */
    init_values();
  
